a07b0149c9c56492e3a68d3c8f87d849fda44420
Brian Cruz <cruz.b.james@gmail.com>
2021-09-25 18:52:02 -0500

    find finInObj

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -163,7 +163,12 @@ Examples:
     filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
-function filterByValue(arr, key) { }
+function filterByValue(arr, key) {
+    const newArr = arr.filter(function (val) {
+        return val[key] !== undefined;
+    })
+    return newArr;
+}
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -173,7 +178,14 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) { }
+function find(arr, searchValue) {
+    if (arr.indexOf(searchValue) !== -1) {
+        return searchValue;
+    }
+    else {
+        return undefined;
+    }
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
@@ -182,7 +194,13 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) { }
+function findInObj(arr, key, searchValue) {
+    for (obj of arr) {
+        if (obj[key] === searchValue) {
+            return obj;
+        }
+    }
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
